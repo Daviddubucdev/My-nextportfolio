b9c7c807b6b29d89539accc6acf9dd4b36ddbec8
David <dubuc.dev@gmail.com>
2024-03-28 18:28:56 -0400

    v4 update

--- app/components/bgmiddlepage.tsx
+++ app/components/bgmiddlepage.tsx
@@ -3,18 +3,11 @@ import JSmosaic from '../public/images/backgroundft.png';
 
 export default function BannerJS() {
     return (
-
-
-        <div className='main-bottom'>
-            <Image
-                src={JSmosaic}
-                alt="picture of enumeration of tech in front-end world"
-                layout="responsive"
-                quality={100}
-                width={1920}
-
-            />
-        </div>
-
+        <Image
+            src={JSmosaic}
+            alt="picture of enumeration of tech in front-end world"
+            layout="responsive"
+            quality={100}
+        />
     );
 }
