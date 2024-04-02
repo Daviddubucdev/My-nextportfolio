7f8cef910f4b57448f0460687210f9c694711b1d
David <dubuc.dev@gmail.com>
2024-04-02 13:54:04 -0400

    txt/about

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -7,7 +7,16 @@ export default function about() {
             <main className="flex max-h-screen flex-col items-center justify-around px-10 pt-10 ">
                 <h1 className="text-blue-400 text-3xl" >About David Dubuc</h1>
                 <section className="border-blue-300">
+                    <div>
 
+                        <h2>Experience</h2>
+                        <p>I have been working in the web development industry for several years, honing my skills in HTML, CSS, and JavaScript. I have a keen eye for design and a strong understanding of front-end frameworks like React and Angular.</p>
+
+                        <h2>Contact</h2>
+                        <p>If you'd like to get in touch or discuss a project, feel free to reach out to me at dubuc.dev@gmail.com. I'm always excited to collaborate and bring ideas to fruition.</p>
+
+
+                    </div>
                 </section>
                 <div className="place-self-end pt-20 mr-20 h-0 opacity-75">
                     <Gmap />
